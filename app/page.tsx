db315e332e09a4d042dfdf09958314165626cb81
hiraokadaisuke <d.hiraoka@samba.ocn.ne.jp>
2025-06-23 14:43:08 +0900

    トップページにリンク追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <main>
+    <main style={{ padding: '2rem' }}>
       <h1>ようこそ！在庫管理ソフトへ</h1>
       <p>このページは仮のトップページです。</p>
+
+      <p style={{ marginTop: '2rem' }}>
+        👉 <Link href="/admin/inventory">在庫一覧ページへ移動</Link>
+      </p>
     </main>
   )
 }
